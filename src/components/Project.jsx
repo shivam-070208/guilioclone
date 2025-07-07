e07e163d4af303e5553a60dd97625bf62c0b37f6
shvgpt-070208 <shvgpt070208@gmail.com>
2025-07-07 15:14:31 +0530

    seo commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -59,41 +59,7 @@ const projects = [
 ]
   return (
     <section id="projects" className="min-h-screen w-screen px-2 pb-4">
-      <div id="projectinstance" className=" w-screen min-h-screen ">
-        <div className="w-full h-screen  md:pl-10 px-2 md:pr-10 pt-20 flex flex-col">
-        <Text text={"Project"} />
-          <p className="w-full max-w-md text-xl h-fit self-end mt-20  text-[var(--color-primary)]">
-            {"I love to build Creative Fullstack web with Emerging Experience."
-              .split("")
-              .map((item, i) => (
-                <span
-                  key={i}
-                  className="h-fit py-[0.6px]  overflow-hidden inline-block  font-semibold text-[18.5px]"
-                >
-                  <motion.span
-                    initial={{ y: 26 }}
-                    whileInView={{ y: 0 }}
-                    viewport={{ margin: "-20px" }}
-                    transition={{ delay: 0.01 * i }}
-                    className="block"
-                  >
-                    {" "}
-                    {item === " " ? "\u00A0" : item}
-                  </motion.span>
-                </span>
-              ))}
-          </p>
-
-          <motion.p
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: [10, 0, 10] }}
-            transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-            className="text-white text-sm sm:text-base tracking-widest self-end underline  uppercase"
-          >
-            Scroll to View
-          </motion.p>
-        </div>
-      </div>
+   
        <section id="showcase" className="min-h-screen relative w-screen  ">
     <Projectcanvas />
    
